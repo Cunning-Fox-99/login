e6f3378dd88941b4ec4bb4890c25bcae46ac9d05
Cunning-Fox-99 <kostya.moroz.96@gmail.com>
2021-10-13 15:55:02 +0300

    fixes

---
+++ src/js/views/form.js
@@ -0,0 +1,23 @@
+function inputErrorTepmpalte(msg) {
+  return `
+  <div class="invalid-feedback">${msg}</div>
+  `;
+}
+
+export function showInputError(el) {
+  const parent = el.parentElement;
+  const msg = el.dataset.invalidMessage || 'Invalid input';
+  const template = inputErrorTepmpalte(msg);
+  el.classList.add('is-invalid');
+  parent.insertAdjacentHTML('beforeend', template);
+
+}
+
+export function removeInputError(el) {
+  const parent = el.parentElement;
+  const err = parent.querySelector('.invalid-feedback');
+  if (!err) return;
+
+  el.classList.remove('is-invalid');
+  parent.removeChild(err);
+}
