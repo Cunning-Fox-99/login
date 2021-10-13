89cb60e3adc21a1d2734af339be9d66599ae1ac5
Cunning-Fox-99 <kostya.moroz.96@gmail.com>
2021-10-13 16:52:47 +0300

    fixes

--- src/js/config/ui.config.js
+++ src/js/config/ui.config.js
@@ -2,6 +2,6 @@ const UI = {
   form: document.forms['loginForm'],
   inputEmail: document.getElementById('email'),
   inputPassword: document.getElementById('password'),
-}
+};
 
 export default UI;
